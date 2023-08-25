22d559ead17b1ddb5f43016efc982eeaea060b50
Daniel M <dm@versatus.io>
2023-08-25 02:12:37 +0200

    denoise logs

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -246,7 +246,7 @@ impl Node {
 
     /// Handles a request RPC.
     fn handle_request(&mut self, request: &Request) {
-        info!(
+        debug!(
             "{} - Receiving request from {} {:#?}",
             self.node_data.addr, request.sender.addr, request.payload,
         );
